ec88b0f102854a3b5c70d3c4ab370335608f3117
Nate Richman <naterichman@flywheel.io>
2020-12-10 11:00:53 -0600

    MAIN: Add testing for parser

---
+++ tests/test_parser.py
@@ -0,0 +1,17 @@
+import pytest
+
+from unittest.mock import MagicMock
+
+import flywheel
+from flywheel_gear_toolkit import GearToolkitContext
+
+from parser import parse_config
+
+
+def test_parse_config():
+    gear_context = MagicMock(spec=GearToolkitContext)
+
+    parse_config(gear_context)
+
+    gear_context.get_input_path.called_count == 4
+    gear_context.get_input.assert_called_once_with('file-input')
