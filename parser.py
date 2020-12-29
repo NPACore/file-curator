8978c773f899c78a21b7031bcdab907d5e8f5026
Nate Richman <naterichman@flywheel.io>
2020-12-29 11:49:54 -0600

    MAIN: Incorporate review comments: * Remove optional requirements * verbose option --> debug

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from flywheel_gear_toolkit import GearToolkitContext
 
 def parse_config(
     gear_context: GearToolkitContext,
-) -> Tuple[str, Dict[str, Any], Dict[str, Optional[str]], Optional[str]]:
+) -> Tuple[str, Dict[str, Any], Dict[str, Optional[str]]]:
     """Parse gear config
 
     Args:
@@ -18,18 +18,15 @@ def parse_config(
                 - (str) path to curator
                 - (Dict[str,Any]) input file
                 - (Dict[str,Optional[str]]) Dictionary of optional additional inputs
-                - (Optional[str]) optional requirements to install
     """
 
     curator_path = gear_context.get_input_path("curator")
-    optional_requirements = None
-    optional_requirements = gear_context.get_input_path("optional-requirements")
 
     file_input = gear_context.get_input("file-input")
     input_file_one = gear_context.get_input_path("additional-input-one")
     input_file_two = gear_context.get_input_path("additional-input-two")
     input_files = {
-        "input_file_one": input_file_one,
-        "input_file_two": input_file_two,
+        "additional_input_one": input_file_one,
+        "additional_input_two": input_file_two,
     }
-    return curator_path, file_input, input_files, optional_requirements
+    return curator_path, file_input, input_files 
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import parser
 import flywheel
 from flywheel_gear_toolkit import GearToolkitContext
 from flywheel_gear_toolkit.utils import datatypes, install_requirements
-from flywheel_hierarchy_curator.curate import get_curator
+from flywheel_gear_toolkit.utils import get_curator
 
 log = logging.getLogger(__name__)
 
@@ -26,12 +26,7 @@ def main(gear_context: GearToolkitContext) -> None: # pragma: no cover
         curator_path,
         file_input,
         optional_inputs,
-        optional_requirements,
     ) = parser.parse_config(gear_context)
-
-    if optional_requirements:
-        log.info(f"Installing requirements from {optional_requirements}")
-        install_requirements(optional_requirements)
 
     log.info(f"Curating {file_input.get('location').get('name')}")
 
@@ -39,16 +34,11 @@ def main(gear_context: GearToolkitContext) -> None: # pragma: no cover
         gear_context,
         file_input,
         curator_path,
-        write_report=gear_context.config.get("write_report"),
         **optional_inputs,
     )
 
 
 if __name__ == "__main__": # pragma: no cover
     with GearToolkitContext() as gear_context:
-        gear_context.init_logging(
-            default_config_name=(
-                "debug" if gear_context.config.get("verbose") else "info"
-            )
-        )
+        gear_context.init_logging()
         main(gear_context)
