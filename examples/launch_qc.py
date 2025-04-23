acd9bfc173d0909731da3cb83e3334c1e13cae41
WillForan <willforan@gmail.com>
2025-04-23 09:38:42 -0400

    WIP: launch_qc.py to set HPC tag and launch

---
+++ examples/launch_qc.py
@@ -0,0 +1,42 @@
+"""
+Launch mrrcqa gear with HPC tag via gear rule
+"""
+
+import logging
+import random
+import re
+import zipfile
+from pathlib import Path
+from typing import Any, Dict
+
+import pydicom
+import flywheel
+from flywheel_gear_toolkit.utils.curator import FileCurator
+from flywheel_gear_toolkit.utils.reporters import AggregatedReporter
+
+log = logging.getLogger("launchHPC")
+log.setLevel("DEBUG")
+
+def launch_gear(fw, file_: flywheel.FileEntry):
+    gear = fw.lookup("gears/mrrcqa")
+    container = file_.parent
+    log.info("gear %s:%s into parent contianer %s", gear.gear.name, gear.gear.version, container.label)
+
+    inputs = {"phantom_dicom": file_}
+    config = {"write_db": True}
+    tags = ["mrrcqa", "hpc"]
+    job_id = gear.run(tags=tags, config=config, inputs=inputs, destination=container)
+    return job_id
+
+class Curator(FileCurator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.reporter = None
+
+    def curate_file(self, file_: flywheel.FileEntry): #Dict[str, Any]):
+        log.info("looking at %s", file_.name)
+        if not re.search(r'.dicom.zip$', file_.name):
+            log.info("file '%s' is not a dicom archive! not running!", file_.name)
+            return
+        jobid = launch_gear(self.client, file_)
+        log.info("launched %d", jobid)
