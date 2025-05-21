4aedb0486a29cdabbfd2231eb3ee21020e60f697
WillForan <willforan@gmail.com>
2025-05-21 14:46:10 -0400

    🐛 launch_qc.py: use dcm within files array for gear input

--- examples/launch_qc.py
+++ examples/launch_qc.py
@@ -3,13 +3,9 @@ Launch mrrcqa gear with HPC tag via gear rule
 """
 
 import logging
-import random
 import re
-import zipfile
-from pathlib import Path
 from typing import Any, Dict
 
-import pydicom
 import flywheel
 from flywheel_gear_toolkit.utils.curator import FileCurator
 from flywheel_gear_toolkit.utils.reporters import AggregatedReporter
@@ -17,16 +13,29 @@ from flywheel_gear_toolkit.utils.reporters import AggregatedReporter
 log = logging.getLogger("launchHPC")
 log.setLevel("DEBUG")
 
-def launch_gear(fw, file_: flywheel.FileEntry):
-    gear = fw.lookup("gears/mrrcqa")
-    container = file_.parent
-    log.info("gear %s:%s into parent contianer %s", gear.gear.name, gear.gear.version, container.label)
+def launch_gear(fw, file_entity: flywheel.FileEntry) -> str:
+    """
+    Launch MRRC QA with "hpc" tag.
+    :param fw: client
+    :param file_entity: entity w/ files array that includes dicom zip
+    :return: jobid of new job
+    """
+    #log.info("input file: %s", file_entity)
 
-    inputs = {"phantom_dicom": file_}
+    dcm = [f for f in file_entity.files if f.mimetype == 'application/zip']
+    if len(dcm) != 1:
+        raise Exception(f"Expect single dicom zip. got {len(dcm)}")
+    file = dcm[0]
+
+    gear = fw.lookup("gears/mrrcqa")
+    log.info("gear %s:%s into %s", gear.gear.name, gear.gear.version, file_entity.label)
+
+    inputs = {"phantom_dicom": file}
     config = {"write_db": True}
     tags = ["mrrcqa", "hpc"]
-    job_id = gear.run(tags=tags, config=config, inputs=inputs, destination=container)
+    job_id = gear.run(tags=tags, config=config, inputs=inputs, destination=file_entity)
     return job_id
+
 
 class Curator(FileCurator):
     def __init__(self, **kwargs):
@@ -47,5 +56,10 @@ class Curator(FileCurator):
 
         file_id = file_['hierarchy']['id']
         file_entity = self.client.get(file_id)
+        if not file_entity:
+            log.error("Bad file ID? %s", file_['hierarchy']['id'])
+            raise Exception("no file")
+
         jobid = launch_gear(self.client, file_entity)
-        log.info("launched %d", jobid)
+        # Detail: [{'type': 'missing', 'loc': ['body', 'inputs', 'phantom_dicom', 'FileReference', 'name'], 'msg': 'Field required', 'input': {'type': 'acquisition', 'id': '680b674a5e18231e7a65fc0f'}}, {'type': 'missing', 'loc': ['body', 'inputs', 'phantom_dicom', 'FileVersion', 'file_id'], 'msg': 'Field required', 'input': {'type': 'acquisition', 'id': '680b674a5e18231e7a65fc0f'}}]
+        log.info("launched %s", jobid)
